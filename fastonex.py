363ab558a820ff11b945837cfb266e40c93d5286
fonors <thea_martian@proton.me>
2024-05-20 17:09:49 +0100

    Output NEXUS conversion to STDOUT

--- fastonex.py
+++ fastonex.py
@@ -16,6 +16,9 @@ def user_args(argparser):
     return user_args
 
 def fasta_todict(file):
+    """
+    Takes a FASTA formatted file and returns a dictionary with the sequences.
+    """
     with open(file, "r") as file:
         sequences = {}
         for line in file:
@@ -29,28 +32,54 @@ def fasta_todict(file):
     return sequences
 
 def nexus_data(seqdict):
+    """
+    Takes a dictionary containing sequences and returns the NEXUS DATA header.
+    """
     nexus_header = "#NEXUS\n\n"
     nexus_header = nexus_header + "BEGIN DATA;\n"
-    nexus_header = nexus_header + f"DIMENSIONS NTAX={str(len(seqdict))} NCHAR={str(maxseqlen(inputfile))};\n"
-    if isdna:
-        newnexusfile.write("FORMAT DATATYPE=DNA MISSING=N GAP=-;\n")
-    else:
-        newnexusfile.write("FORMAT DATATYPE=RNA MISSING=N GAP=-;\n")
 
-def nexus_matrix():
-    newnexusfile.write("MATRIX\n\n")
+    seqlist = []
     for seq in seqdict:
-        if len(seqdict[seq]) < maxseqlength:
-            ngaps = maxseqlength - len(seqdict[seq])
-            newnexusfile.write(f"{seq}     {seqdict[seq]}")
+        seqlist.append(seqdict[seq])
+    maxseqlen = max(len(seq) for seq in seqlist)
+    nexus_header = nexus_header + f"DIMENSIONS NTAX={str(len(seqdict))} NCHAR={str(maxseqlen))};\n"
+
+    for seq in seq_dict:
+        current_sequence = seq_dict[seq].upper()
+        if "U" in current_sequence:
+            nexus_header = nexus_header + "FORMAT DATATYPE=RNA MISSING=N GAP=-;\n"
+        elif "T" in current_sequence:
+            nexus_header = nexus_header + "FORMAT DATATYPE=DNA MISSING=N GAP=-;\n"
+
+    return nexus_header
+
+def nexus_matrix(seqdict):
+    """
+    Takes a dictionary containing sequences and returns the NEXUS MATRIX block.
+    """
+    seqlist = []
+    for seq in seqdict:
+        seqlist.append(seqdict[seq])
+    maxseqlen = max(len(seq) for seq in seqlist)
+
+    nexus_matrix_block = "MATRIX\n\n"
+    for seq in seqdict:
+        if len(seqdict[seq]) < maxseqlen:
+            ngaps = maxseqlen - len(seqdict[seq])
+            nexus_matrix_block = nexus_matrix_block + f"{seq}     {seqdict[seq]}")
             for gaps in range(ngaps):
-                newnexusfile.write("-")
-            newnexusfile.write("\n")
+                nexus_matrix_block = nexus_matrix_block + "-"
+            nexus_matrix_block = nexus_matrix_block + "\n"
         else:
-            newnexusfile.write(f"{seq}     {seqdict[seq]}\n")
-    newnexusfile.write(";\n")
-    newnexusfile.write("\nEND;")
+            nexus_matrix_block = nexus_matrix_block + f"{seq}     {seqdict[seq]}\n"
+    nexus_matrix_block = nexus_matrix_block + ";\n"
+    nexus_matrix_block = nexus_matrix_block + "\nEND;"
+
+    return nexus_matrix_block
 
 if __init__ == "__name__":
     user_args = user_args(parser)
     seq_dict = fasta_todict(user_args.input)
+    output = nexus_data(seq_dict)
+    output = output + nexus_matrix(seq_dict)
+    print(output)
